3cb57e5270307f16660c1baad04a6d973dbe6673
Italo Aurelio <italoaurelior@gmail.com>
2018-11-17 18:26:16 -0200

    Update persons.js

--- persons.js
+++ persons.js
@@ -1,5 +1,59 @@
-{
-    name: "João Pedro",
-    gituser: "https://github.com/jpmuniz",
-    linkedin: "https://www.linkedin.com/in/jo%C3%A3o-pedro-00939867/"    
-  },
+ export const persons = [
+    {
+      name: "Italo Aurelio",
+      gituser: "italox" ,
+      linkedin: "http://linkedin.com.br"    
+    },
+    {
+      name: "RocketSeat",
+      gituser: "rocketseat" ,
+      linkedin: "http://linkedin.com.br"    
+    },
+    {
+      name: "Cleiton Souze",
+      gituser: "cleitonsouza" ,
+      linkedin: "http://linkedin.com.br"    
+    },
+    {
+      name: "Claudio junior",
+      gituser: "cso01" ,
+      linkedin: "http://linkedin.com.br"    
+    },
+    {
+      name: "Diego Fernandes",
+      gituser: "diego3g" ,
+      linkedin: "http://linkedin.com.br"    
+    },
+    {
+      name: "Robson Marques",
+      gituser: "robsonmarques",
+      linkedin: "http://linkedin.com.br"    
+    },
+    {
+      name: "Bruno Frigeri",
+      gituser: "brunofrigeri",
+      linkedin: "https://www.linkedin.com/in/bruno-frigeri-b8b29a175/"    
+    },    
+    {
+      name: "João Paulo de Magalhães ",
+      gituser: "jpdemagalhaes",
+      linkedin: "http://linkedin.com.br"    
+    },
+    {
+      name: "Guilherme Pellizzetti",
+      gituser: "pellizzetti",
+      linkedin: "http://linkedin.com.br"    
+    },
+    {
+      name: "Pedro Oliveira",
+      gituser: "Axobirosbaldo",
+      linkedin: "https://www.linkedin.com/in/pedro-lopes-de-oliveira-645438125"    
+    },
+    {
+      name: "João Pedro",
+      gituser: "jpmuniz",
+      linkedin: "https://www.linkedin.com/in/jo%C3%A3o-pedro-00939867/"    
+    },
+
+];
+  
