06b1b69e0435323762f12e5ef84097360a172d23
Italo <italoaurelior@gmail.com>
2018-11-17 08:16:26 -0200

    ajustando titulo e api

--- src/services/api.js
+++ src/services/api.js
@@ -1,11 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({ 
-    baseURL: "https://api.github.com/users",
-    auth: {
-        client_id: process.env.GIT_API_ID,
-        client_secret: process.env.GIT_API_SECRET
-    },
+    baseURL: "https://api.github.com/users"
 });
 
 export default api;
