52b08275954c46fd919ed1e2af7d361b65d627bb
Marcio Angelo Matté <marcioamatte@gmail.com>
2018-11-19 13:22:20 -0200

    Adicionando meu nome - Marcio Angelo Matté - à lista

--- src/components/persons.js
+++ src/components/persons.js
@@ -1,87 +1,91 @@
-export const persons = [
-    {
-      name: "Italo Aurelio",
-      gituser: "italox" ,
-      linkedin: "http://linkedin.com.br"    
-    },
-    {
-      name: "RocketSeat",
-      gituser: "rocketseat" ,
-      linkedin: "http://linkedin.com.br"    
-    },
-    {
-      name: "Cleiton Souze",
-      gituser: "cleitonsouza" ,
-      linkedin: "http://linkedin.com.br"    
-    },
-    {
-      name: "Claudio junior",
-      gituser: "cso01" ,
-      linkedin: "http://linkedin.com.br"    
-    },
-    {
-      name: "Diego Fernandes",
-      gituser: "diego3g" ,
-      linkedin: "http://linkedin.com.br"    
-    },
-    {
-      name: "Robson Marques",
-      gituser: "robsonmarques",
-      linkedin: "http://linkedin.com.br"    
-    },
-    {
-      name: "Bruno Frigeri",
-      gituser: "brunofrigeri",
-      linkedin: "https://www.linkedin.com/in/bruno-frigeri-b8b29a175/"    
-    },    
-    {
-      name: "João Paulo de Magalhães ",
-      gituser: "jpdemagalhaes",
-      linkedin: "http://linkedin.com.br"    
-    },
-    {
-      name: "Guilherme Pellizzetti",
-      gituser: "pellizzetti",
-      linkedin: "http://linkedin.com.br"    
-    },
-    {
-      name: "Pedro Oliveira",
-      gituser: "Axobirosbaldo",
-      linkedin: "https://www.linkedin.com/in/pedro-lopes-de-oliveira-645438125"    
-    },
-    {
-      name: "João Pedro",
-      gituser: "jpmuniz",
-      linkedin: "https://www.linkedin.com/in/jo%C3%A3o-pedro-00939867/"    
-    },  
-    {
-      name: "Lucas Fernandes Sandin",
-      gituser: "lucassandin",
-      linkedin: "https://www.linkedin.com/in/lucas-fernandes-sandin-974488172/"    
-    },    
-    {
-      name: "Italo Izaac",
-      gituser: "iiandrade",
-      linkedin: "https://www.linkedin.com/in/iiandrade/"
-    },
-    {
-      name: "André Nunes",
-      gituser: "AndreNunes1812",
-      linkedin: "www.linkedin.com/in/andrenunes18"
-    },
-    {
-      name: "Stênia Felix",
-      gituser: "steniafelix",
-      linkedin: "https://www.linkedin.com/in/steniafelix/"
-    },
-    {
-      name: "Abimael Andrade",
-      gituser: "abimaelandrade",
-      linkedin: "https://www.linkedin.com/in/abimaelandrade/"
-    },
-    {
-      name: "Vinicius Meneses",
-      gituser: "viniciusmeneses",
-      linkedin: "https://www.linkedin.com/in/vinicius-meneses/"
-    },
+export const persons = [{
+    name: "Italo Aurelio",
+    gituser: "italox",
+    linkedin: "http://linkedin.com.br"
+  },
+  {
+    name: "RocketSeat",
+    gituser: "rocketseat",
+    linkedin: "http://linkedin.com.br"
+  },
+  {
+    name: "Cleiton Souze",
+    gituser: "cleitonsouza",
+    linkedin: "http://linkedin.com.br"
+  },
+  {
+    name: "Claudio junior",
+    gituser: "cso01",
+    linkedin: "http://linkedin.com.br"
+  },
+  {
+    name: "Diego Fernandes",
+    gituser: "diego3g",
+    linkedin: "http://linkedin.com.br"
+  },
+  {
+    name: "Robson Marques",
+    gituser: "robsonmarques",
+    linkedin: "http://linkedin.com.br"
+  },
+  {
+    name: "Bruno Frigeri",
+    gituser: "brunofrigeri",
+    linkedin: "https://www.linkedin.com/in/bruno-frigeri-b8b29a175/"
+  },
+  {
+    name: "João Paulo de Magalhães ",
+    gituser: "jpdemagalhaes",
+    linkedin: "http://linkedin.com.br"
+  },
+  {
+    name: "Guilherme Pellizzetti",
+    gituser: "pellizzetti",
+    linkedin: "http://linkedin.com.br"
+  },
+  {
+    name: "Pedro Oliveira",
+    gituser: "Axobirosbaldo",
+    linkedin: "https://www.linkedin.com/in/pedro-lopes-de-oliveira-645438125"
+  },
+  {
+    name: "João Pedro",
+    gituser: "jpmuniz",
+    linkedin: "https://www.linkedin.com/in/jo%C3%A3o-pedro-00939867/"
+  },
+  {
+    name: "Lucas Fernandes Sandin",
+    gituser: "lucassandin",
+    linkedin: "https://www.linkedin.com/in/lucas-fernandes-sandin-974488172/"
+  },
+  {
+    name: "Italo Izaac",
+    gituser: "iiandrade",
+    linkedin: "https://www.linkedin.com/in/iiandrade/"
+  },
+  {
+    name: "André Nunes",
+    gituser: "AndreNunes1812",
+    linkedin: "www.linkedin.com/in/andrenunes18"
+  },
+  {
+    name: "Stênia Felix",
+    gituser: "steniafelix",
+    linkedin: "https://www.linkedin.com/in/steniafelix/"
+  },
+  {
+    name: "Abimael Andrade",
+    gituser: "abimaelandrade",
+    linkedin: "https://www.linkedin.com/in/abimaelandrade/"
+  },
+  {
+    name: "Vinicius Meneses",
+    gituser: "viniciusmeneses",
+    linkedin: "https://www.linkedin.com/in/vinicius-meneses/"
+  },
+  {
+    name: "Marcio Angelo Matté",
+    gituser: "marcioamatte",
+    linkedin: "https://www.linkedin.com/in/marcioam/"
+  },
 ];
