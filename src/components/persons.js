5f120aa9c500de19705e23d94b30f0555b11179d
Italo Aurelio <italoaurelior@gmail.com>
2018-11-18 22:04:52 -0200

    Update persons.js

--- src/components/persons.js
+++ src/components/persons.js
@@ -53,6 +53,16 @@ export const persons = [
       name: "João Pedro",
       gituser: "jpmuniz",
       linkedin: "https://www.linkedin.com/in/jo%C3%A3o-pedro-00939867/"    
+    },  
+    {
+      name: "Lucas Fernandes Sandin",
+      gituser: "lucassandin",
+      linkedin: "https://www.linkedin.com/in/lucas-fernandes-sandin-974488172/"    
+    },    
+    {
+      name: "Italo Izaac",
+      gituser: "iiandrade",
+      linkedin: "https://www.linkedin.com/in/iiandrade/"
     },
     {
       name: "André Nunes",
