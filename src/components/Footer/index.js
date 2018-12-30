fcb6c1c54db7308872045580356b3d4926325fa3
ChilenoSika <jaum97@yahoo.com.br>
2018-12-30 03:23:49 -0200

    Transformando algumas classes em functions

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,13 +1,11 @@
-import React, { Component } from 'react';
-import { Layout } from 'antd';
+import React from "react";
+import { Layout } from "antd";
 const { Footer } = Layout;
 
-export default class FooterDev extends Component {
-    render() {
-      return (
-        <Footer style={{ textAlign: 'center' }}>
-            RocketSeat Devs ©2018 Created by @italox
-        </Footer>
-      );
-    }
-  }
+const FooterDev = () => (
+  <Footer style={{ textAlign: "center" }}>
+    RocketSeat Devs ©2018 Created by @italox
+  </Footer>
+);
+
+export default FooterDev;
